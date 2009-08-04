f9300b38a90141cc27393d3ddbeec7d7c333dda5
Ian Obermiller <ian@obermillers.com>
2009-08-04 13:52:27 +0000

    Fixed walking speed and health display. Added logout. Added basic talking (you see yourself talk).

---
+++ Packets/Client/PlayerSpeechPacket.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpOT.Packets
+{
+    public class PlayerSpeechPacket : Packet
+    {
+        public SpeechType SpeechType { get; set; }
+        public string Receiver { get; set; }
+        public string Message { get; set; }
+        public ChatChannel ChannelId { get; set; }
+
+        public static PlayerSpeechPacket Parse(NetworkMessage message)
+        {
+            PlayerSpeechPacket packet = new PlayerSpeechPacket();
+
+            packet.SpeechType = (SpeechType)message.GetByte();
+
+            switch (packet.SpeechType)
+            {
+                case SpeechType.Private:
+                case SpeechType.PrivateRed:
+                case SpeechType.RuleViolationAnswer:
+                    packet.Receiver = message.GetString();
+                    break;
+                case SpeechType.ChannelYellow:
+                case SpeechType.ChannelRed:
+                case SpeechType.ChannelRedAnonymous:
+                case SpeechType.ChannelWhite:
+                    packet.ChannelId = (ChatChannel)message.GetUInt16();
+                    break;
+                default:
+                    break;
+            }
+
+            packet.Message = message.GetString();
+
+            return packet;
+        }
+    }
+}
---
+++ Packets/Server/CreatureSpeechPacket.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpOT.Packets
+{
+    public class CreatureSpeechPacket : Packet
+    {
+        public static void Add
+        (
+            NetworkMessage message, 
+            string senderName, 
+            ushort senderLevel, 
+            SpeechType speechType, 
+            string text, 
+            Location location, 
+            ChatChannel channelId, 
+            uint time
+        )
+        {
+            message.AddByte((byte)ServerPacketType.CreatureSpeech);
+
+            message.AddUInt32(0x00000000);
+            message.AddString(senderName);
+            message.AddUInt16(senderLevel);
+            message.AddByte((byte)speechType);
+
+            switch (speechType)
+            {
+                case SpeechType.Say:
+                case SpeechType.Whisper:
+                case SpeechType.Yell:
+                case SpeechType.MonsterSay:
+                case SpeechType.MonsterYell:
+                case SpeechType.PrivateNPCToPlayer:
+                    message.AddLocation(location);
+                    break;
+                case SpeechType.ChannelRed:
+                case SpeechType.ChannelRedAnonymous:
+                case SpeechType.ChannelOrange:
+                case SpeechType.ChannelYellow:
+                case SpeechType.ChannelWhite:
+                    message.AddUInt16((ushort)channelId);
+                    break;
+                case SpeechType.RuleViolationReport:
+                    message.AddUInt32(time);
+                    break;
+                default:
+                    break;
+
+            }
+
+            message.AddString(text);
+        }
+    }
+}
