40b2f9a99c3e9d061f224728bffccd6400931e08
Ian Obermiller <ian@obermillers.com>
2009-08-21 19:32:06 +0000

    Added custom MapTracker.

---
+++ MapTracker/MapTile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Tibia;
+using Tibia.Objects;
+
+namespace SharpOTMapTracker
+{
+    public class MapTile
+    {
+        public Location Location;
+        public ushort TileId;
+        public List<MapItem> Items = new List<MapItem>();
+    }
+}
