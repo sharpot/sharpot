acf91334eef9f96a1ed0d11b9cf20fb1743d3947
ian320 <ian320@9c762d06-7ae1-11de-888d-2103c0e6b660>
2009-08-26 17:07:53 +0000

    Added timing, colors, and exception handling to the loading sequence.

--- Util/Extensions.cs
+++ Util/Extensions.cs
@@ -7,6 +7,22 @@ namespace SharpOT.Util
 {
     public static class Extensions
     {
+        /// <summary>
+        /// Repeats the specified string n times.
+        /// </summary>
+        /// <param name="instr">The input string.</param>
+        /// <param name="n">The number of times input string 
+        /// should be repeated.</param>
+        /// <returns></returns>
+        // http://weblogs.asp.net/gunnarpeipman/archive/2009/05/13/string-repeat-smaller-and-faster-version.aspx
+        public static string Repeat(this string instr, int n)
+        {
+            if (string.IsNullOrEmpty(instr))
+                return instr;
+
+            var result = new StringBuilder(instr.Length * n);
+            return result.Insert(0, instr, n).ToString();
+        }
 
         #region Packets
 
