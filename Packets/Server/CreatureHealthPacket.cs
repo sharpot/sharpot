d0f69a468a46ec233365dc16017ac74c7907e051
DarkstaR6925 <DarkstaR6925@gmail.com@9c762d06-7ae1-11de-888d-2103c0e6b660>
2009-09-08 05:18:55 +0000

    Creature health packet added

---
+++ Packets/Server/CreatureHealthPacket.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpOT.Packets
+{
+    public class CreatureHealthPacket : Packet
+    {
+        public static void Add
+       (
+           NetworkMessage message,
+           uint creatureID,
+           byte newHealth
+       )
+        {
+            message.AddByte((byte)ServerPacketType.CreatureHealth);
+
+            message.AddUInt32(creatureID);
+            message.AddByte(newHealth);
+        }
+    }
+}
