49500fd61b73466691f1c263e6e0ff53ea24597c
DarkstaR6925 <DarkstaR6925@gmail.com@9c762d06-7ae1-11de-888d-2103c0e6b660>
2009-09-12 14:47:42 +0000

    Redid Items class, added Container and Weapon, which both inherit Item

--- Items/Item.cs
+++ Items/Item.cs
@@ -10,18 +10,14 @@ namespace SharpOT
         //The plan is to make a list of every item from Items.XML, and then index
         //through it when a new item is created.
         //This will also be used for items on the map or in a players inventory, ofcourse
+        public double Weight { get; set; }
         public ushort Id;
         public byte Extra;
         public Location Location { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string SpecialDescription { get; set; }
-        public double Weight { get; set; }
-        public ushort Attack { get; set; }
-        public ushort Defense { get; set; }
         public ushort Armor { get; set; }
-        public short ExtraAttack { get; set; }
-        public short ExtraDefense { get; set; }
 
 
         #region "Constructors"
@@ -57,18 +53,17 @@ namespace SharpOT
         }
 
         #region "LookAt methods"
-        public void LookAt(Player player)
+        protected virtual void LookAt(Player player)
         {
             if (player.Tile.Location.CanSee(Location))
             {
                 player.Connection.SendTextMessage(TextMessageType.DescriptionGreen,
                 "You see " + GetArticle(Name) + " " + Name +//Name
-                GetCombatAttributes() + Description + SpecialDescription +//Attributes and description
+                Description + SpecialDescription + Description +
                 "\n It weighs " + Weight + " oz.");//Weight
             }
         }
-
-        private string GetArticle(string text)
+        public string GetArticle(string text)
         {
             if (text.Length == 0) return string.Empty;
 
@@ -79,40 +74,6 @@ namespace SharpOT
             }
 
             return "a";
-        }
-
-        private string GetCombatAttributes()
-        {
-            string temp = "";
-            if (Attack > 0 && Defense > 0)
-            {
-                //Add the items attack
-                temp = "(Atk:" + Attack;
-                if (ExtraAttack > 0)
-                {
-                    temp += " +" + ExtraAttack;
-                }
-                else if (ExtraAttack < 0)
-                {
-                    temp += " -" + Math.Abs(ExtraAttack);
-                }
-                //Add the Defense
-                temp += ", Def:" + Defense;
-                if (ExtraDefense > 0)
-                {
-                    temp += " +" + ExtraDefense;
-                }
-                else if (ExtraDefense < 0)
-                {
-                    temp += " -" + Math.Abs(ExtraDefense);
-                }
-                temp += ")";
-            }
-            else if (Armor > 0)
-            {
-                temp = "(Arm:" + Armor + ")";
-            }
-            return temp;
         }
         #endregion
 
