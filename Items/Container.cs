a55f5b0e9823a7818473b425dd2d63ebdbd7ea09
DarkstaR6925 <DarkstaR6925@gmail.com@9c762d06-7ae1-11de-888d-2103c0e6b660>
2009-09-12 14:47:42 +0000

    Redid Items class, added Container and Weapon, which both inherit Item

---
+++ Items/Container.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpOT
+{
+    class Container : Item
+    {
+        public byte Volume { get; set; }
+        public List<Item> Items { get; set; }
+
+
+         protected override void LookAt(Player player)
+        {
+            if (player.Tile.Location.CanSee(Location))
+            {
+                player.Connection.SendTextMessage(
+                    TextMessageType.DescriptionGreen,
+                    "You see " + GetArticle(Name) + " " + Name +
+                    ". (Vol:" + Volume +
+                    Description + SpecialDescription +
+                    "\n It weighs " + (Weight += Items.Sum(P => P.Weight)) + " oz.");
+            }
+        }
+    }
+}
---
+++ Items/Weapon.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpOT
+{
+    class Weapon : Item
+    {
+        public ushort Attack { get; set; }
+        public ushort Defense { get; set; }
+        public short ExtraAttack { get; set; }
+        public short ExtraDefense { get; set; }
+
+
+        protected override void LookAt(Player player)
+        {
+            if (player.Tile.Location.CanSee(Location))
+            {
+                player.Connection.SendTextMessage(TextMessageType.DescriptionGreen,
+                    "You see " + GetArticle(Name) + " " + Name +
+                    ". " + GetCombatAttributes() + Description + SpecialDescription +
+                    "\n It weighs " + Weight + " oz.");
+            }
+        }
+
+        private string GetCombatAttributes()
+        {
+            string temp = "";
+            //Add the items attack
+            temp = "(Atk:" + Attack;
+            if (ExtraAttack > 0)
+            {
+                temp += " +" + ExtraAttack;
+            }
+            else if (ExtraAttack < 0)
+            {
+                temp += " -" + Math.Abs(ExtraAttack);
+            }
+            //Add the Defense
+            temp += ", Def:" + Defense;
+            if (ExtraDefense > 0)
+            {
+                temp += " +" + ExtraDefense;
+            }
+            else if (ExtraDefense < 0)
+            {
+                temp += " -" + Math.Abs(ExtraDefense);
+            }
+            temp += ")";
+            return temp;
+        }
+    }
+}
