714b4cdeb8d4b37daa76b105e8d8d898c8850fbf
ian320 <ian320@9c762d06-7ae1-11de-888d-2103c0e6b660>
2009-11-02 15:01:33 +0000

    Added inventory infrastructure.

---
+++ Packets/Server/InventoryClearSlotPacket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpOT.Packets
+{
+    public class InventoryClearSlotPacket : Packet
+    {
+        public static void Add(NetworkMessage message, SlotType slot)
+        {
+            message.AddByte((byte)ServerPacketType.InventoryClearSlot);
+            message.AddByte((byte)slot);
+        }
+    }
+}
---
+++ Packets/Server/InventorySetSlotPacket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpOT.Packets
+{
+    public class InventorySetSlotPacket : Packet
+    {
+        public static void Add(NetworkMessage message, SlotType slot, Item item)
+        {
+            message.AddByte((byte)ServerPacketType.InventorySetSlot);
+            message.AddByte((byte)slot);
+            message.AddItem(item);
+        }
+    }
+}
